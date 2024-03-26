47f9c746425c0e1c2b4300445a4e1096490a8c83
abdur rhaman babu <abdurrahmanbab74@gamil.com>
2024-03-26 18:58:01 +0600

    hunting done

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
-const phoneHunterLoader = async (searchText, isShowAll) =>{
+const phoneHunterLoader = async (searchText = '13', isShowAll) =>{
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
     const data = await res.json()
     const phones = data.data;
     displayData(phones, isShowAll)
 }
+
+phoneHunterLoader()
 
 const displayData = (phones, isShowAll) =>{
     const main = document.getElementById('main')
@@ -16,7 +18,7 @@ const displayData = (phones, isShowAll) =>{
       showAllItem.classList.add('hidden')
     }
     
-    console.log('show all data', isShowAll)
+    // console.log('show all data', isShowAll)
 
    if(!isShowAll){
       phones = phones.slice(0,12)
@@ -30,10 +32,10 @@ const displayData = (phones, isShowAll) =>{
         <figure><img src= ${phone.image} alt="Shoes" /></figure>
               <div class="card-body">
                   <h2 class="card-title">${phone.brand}</h2>
-                  <h2>${phone.phone_name}</h2>
-                  <p>${phone.slug}</p>
-                  <div class="card-actions justify-end">
-                  <button class="btn btn-primary">Buy Now</button>
+                  <h2 class = "text-2xl">${phone.phone_name}</h2>
+                  <div class="card-actions justify-center my-3">
+                  <button onclick = "showDetails('${phone.slug}')" class="btn btn-primary">Show Details</button>
+                  </div>
               </div>
         `
         main.appendChild(child)
@@ -42,6 +44,28 @@ const displayData = (phones, isShowAll) =>{
     loadingSinner(false)
 }
 
+const showDetails = async (id) =>{
+  const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
+  const data = await res.json()
+  const phones = data.data;
+  console.log(phones)
+  showPhoneDetails(phones)
+  // console.log('show details')
+}
+
+const showPhoneDetails = (phones) =>{
+  show_details_modal.showModal()
+  const phoneName = document.getElementById('phone-name')
+  phoneName.innerText = phones.name;
+  const phoneDetails = document.getElementById('phoneDetails')
+  phoneDetails.innerHTML  = `
+    <img src = ${phones.image} />
+    <p> <span class="text-base font-semibold">Release Date:</span> ${phones.releaseDate}</p>
+    <p> <span class="text-base font-semibold">Memory:</span> ${phones.mainFeatures.memory}</p>
+    <p> <span class="text-base font-semibold">Chip Set:</span> ${phones.mainFeatures.chipSet}</p>
+    <p> <span class="text-base font-semibold">Display Size:</span> ${phones.mainFeatures.displaySize}</p>
+  `
+}
 
 // search result
 const searchInput = document.getElementById('search-input')
